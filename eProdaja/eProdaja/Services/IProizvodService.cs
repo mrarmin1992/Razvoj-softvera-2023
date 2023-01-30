d4eb7f5346385014d4a97a462a994e0078394859
Armin Pezo <arminpezo@Armins-MacBook-Pro.local>
2023-01-30 12:23:22 +0100

    Prvi dio

---
+++ eProdaja/eProdaja/Services/IProizvodService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using eProdaja.Controllers;
+
+namespace eProdaja.Services
+{
+	public interface IProizvodService
+	{
+        public IEnumerable<Proizvod> Get();
+        public Proizvod GetById(int id);
+        public Proizvod Insert(Proizvod proizvod);
+        public Proizvod Update(int id, Proizvod proizvod);
+    }
+}
+
---
+++ eProdaja/eProdaja/Services/ProizvodService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using eProdaja.Controllers;
+
+namespace eProdaja.Services
+{
+	public class ProizvodService : IProizvodService
+	{
+            private static List<Proizvod> _proizvod;
+
+        static ProizvodService()
+        {
+            _proizvod = new List<Proizvod>()
+            {
+                new Proizvod()
+                {
+                    Id=1,
+                    Name="Laptop",
+                },
+                new Proizvod()
+                {
+                    Id=2,
+                    Name="Mobitel",
+                },
+
+            };
+        }
+        public IEnumerable<Proizvod> Get()
+        {
+            return _proizvod;
+        }
+
+        public Proizvod GetById(int id)
+        {
+            return _proizvod.FirstOrDefault(x => x.Id == id);
+        }
+
+        public Proizvod Insert(Proizvod proizvod)
+        {
+            _proizvod.Add(proizvod);
+            return proizvod;
+        }
+
+        public Proizvod Update(int id, Proizvod proizvod)
+        {
+            var current = _proizvod.FirstOrDefault(x => x.Id == id);
+            current.Name = proizvod.Name;
+            return current;
+        }
+
+    }
+}
+
